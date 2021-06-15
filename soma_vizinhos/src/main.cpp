f466daa6c68d7659fb13cef7e7cb60837c33c1e5
Raíssa Santos <raixapsantos@gmail.com>
2021-06-15 18:31:13 -0300

    Terminada

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -10,7 +10,29 @@ using std::endl;
 
 int main( void )
 {
-    // TODO: Adicione seu código aqui. 
+    int m = 0, n = 0, soma, aux;
+
+    while(cin >> m >> n){
+      aux = m;
+      soma = 0;
+
+      if(n > 0){
+        for (int i = 0; i < n; i++) {
+          soma += aux;
+          aux++;
+        }
+      }
+      else if(n < 0) {
+        for (int i = 0; i < (-n); i++) {
+          soma += aux;
+          aux--;
+        }
+      }
+      else
+        soma = m;
+
+      cout << soma << endl;
+    }
 
     return 0;
 }
