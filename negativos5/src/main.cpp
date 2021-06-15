ae00ece6a803322b481a8df2ce550e345bdaf546
Raíssa Santos <raixapsantos@gmail.com>
2021-06-15 15:58:29 -0300

    Terminada

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -7,9 +7,11 @@ const int SIZE = 5; // input size.
 int main(void)
 {
     int N = 0, qtd = 0;
+    
     for (int i = 0; i < SIZE; i++) {
       std::cin >> N;
-      if (N < 0) qtd++;
+      if (N < 0)
+        qtd++;
     }
 
     std::cout << qtd;
