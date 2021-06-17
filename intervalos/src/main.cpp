ed75c74eeee6130c98870d48548ee5a6e82c4e7f
Raíssa Santos <raixapsantos@gmail.com>
2021-06-17 14:40:41 -0300

    Terminada

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -11,11 +11,36 @@ using std::endl;
 #include <iomanip>
 using std::setprecision;
 
-// Se desejar, crie funções aqui, antes do main().
+void verificarIntervalos(int x, int * vec){
+    if(x >= 0 && x < 25)
+      vec[0]++;
+    else if(x >= 25 && x < 50)
+      vec[1]++;
+    else if(x >= 50 && x < 75)
+      vec[2]++;
+    else if(x >= 75 && x < 100)
+      vec[3]++;
+    else if(x < 0 || x >= 100)
+      vec[4]++;
+}
+
+void imprimirPorcentagens(int * vec, double total){ // otimizar o calculo das porcentagens
+    for(int i = 0; i < 5; i++){
+      cout << std::setprecision(4) << (vec[i]/total) * 100 << '\n';
+    }
+}
 
 int main(void)
 {
-    // TODO: Adicione aqui a sua solução.
+    int x, total = 0;
+    int * vetor_intervalos = new int [5];
+
+    while(cin >> std::ws >> x) {
+      total++;
+      verificarIntervalos(x, vetor_intervalos);
+    }
+
+    imprimirPorcentagens(vetor_intervalos, total);
 
     return 0;
 }
