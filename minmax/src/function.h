78aed7df3b7a3fa1f67957d55993aaa9c1c76fee
Raíssa Santos <raixapsantos@gmail.com>
2021-06-18 11:26:34 -0300

    Terminada

--- minmax/src/function.h
+++ minmax/src/function.h
@@ -1,9 +1,10 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
+#include <cstddef>
 #include <utility>
 using std::pair;
 
-std::pair<int, int> min_max( int V[], int n );
+std::pair<int, int> min_max( int V[], size_t n );
 
 #endif
