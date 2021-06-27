772f970d7b25d78a37e7aa75ba71d0d65f2895be
Raíssa Santos <raixapsantos@gmail.com>
2021-06-27 19:24:58 -0300

    Parcial

--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -1,13 +1,22 @@
 #include "function.h"
 
-/*! 
+/*!
  * Verifica se um ponto está dentro de um retângulo.
  */
 
 location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
 {
-    // TODO: Coloque aqui seu código.
-
-    // TODO: Substitua o retorno conforme desejar. Isso é apenas um STUB, usado apenas para compilar.
-    return location_t::OUTSIDE;
+    // dentro -> INSIDE, borda -> BORDER, fora -> OUTSIDE
+    if((P.x == IE.x || P.x == SD.x) && (P.y >= IE.y || P.y <= SD.y)){ // borda: vertical
+        return location_t::BORDER;
+    }
+    else if((P.y == IE.y || P.y == SD.y) && (P.x >= IE.x || P.x <= SD.x)){ // borda: horizontal
+        return location_t::BORDER;
+    }
+    else if((P.x > IE.x && P.x < SD.x) && (P.y > IE.y && P.y < SD.y)){ // dentro
+        return location_t::INSIDE;
+    } // TODO: ajeitar as condições abaixo.
+    else{
+        return location_t::OUTSIDE;
+    }
 }
